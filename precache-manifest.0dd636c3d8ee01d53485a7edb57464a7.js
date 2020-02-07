b7e37f669815faff7ed234c18c57aae00aacfdb3
Soheil Tehrani <44571189+soheiiilst@users.noreply.github.com>
2020-02-07 22:09:09 +0330

    Updates

---
+++ precache-manifest.0dd636c3d8ee01d53485a7edb57464a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a3109615af5cad951477fb1fca9f75",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6169a6b68dc51feaf379",
+    "url": "/monsters-rolodex/static/css/main.67786aba.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6169a6b68dc51feaf379",
+    "url": "/monsters-rolodex/static/js/main.286cb381.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
